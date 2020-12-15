e7bfbddfcdeba8ccb2b328d7682f858f59d6e424
Thom711 <59656747+Thom711@users.noreply.github.com>
2020-12-15 15:06:25 +0100

    -final

---
+++ script.js
@@ -0,0 +1,16 @@
+let naam = "Thom";
+console.log(naam);
+
+console.log(4 + 4);
+console.log("4" + "4");
+
+let sommetje = 4 / 4;
+console.log(sommetje);
+sommetje = 4 * 4 + 4 / 4;
+console.log(sommetje);
+
+let leeftijd = 32;
+console.log(typeof leeftijd);
+
+leeftijd = "32";
+console.log(typeof leeftijd);
